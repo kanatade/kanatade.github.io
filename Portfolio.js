db6291ac6800eab5df5aa3773cfd4b3a450b7831
Daria Zubova <37777402+kanatade@users.noreply.github.com>
2019-08-14 23:38:58 -0700

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,65 @@
+function myFunction1() {
+  var x =
+      document.getElementById("projects");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+
+}
+
+
+function myFunction2() {
+  var x =
+      document.getElementById("my-contacts");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+
+  var y =
+      document.getElementById("btn-contacts");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+
+}
+
+function myFunction3() {
+  var x =
+      document.getElementById("about-me");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+
+ var y =
+      document.getElementById("btn-contacts");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+
+   var q =
+      document.getElementById("my-contacts");
+  if (q.style.visibility === "visible") {
+    q.style.visibility = "hidden";
+  } else {
+    q.style.visibility = "visible";
+  }
+
+   var z =
+      document.getElementById("btn-about");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+
+}
